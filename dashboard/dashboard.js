27b07e188a5043e8ebb01bf9ab0dd723a165676a
Cody Millward <codycmillward@gmail.com>
2025-12-04 11:46:54 -0700

    remove unnecessary things

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -80,27 +80,6 @@ function updateCommandResult(msg) {
         msg.stdout + "\n" + msg.stderr;
 }
 
-// -------------------------------
-// Spectrum Rendering
-// -------------------------------
-
-function updateSpectrum(samples) {
-    const canvas = document.getElementById("spectrumCanvas");
-    const ctx = canvas.getContext("2d");
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.beginPath();
-    samples.forEach((v, i) => {
-        const x = (i / samples.length) * canvas.width;
-        const y = canvas.height - (v * canvas.height);
-        i === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
-    });
-    ctx.strokeStyle = "#44ddee";
-    ctx.lineWidth = 2;
-    ctx.stroke();
-}
-
 function showPanel(name) {
     document.querySelectorAll('.panel').forEach(p => {
         p.classList.remove('active');
